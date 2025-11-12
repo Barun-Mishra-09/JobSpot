e7313baa47154b838870ed40f6065ba1b65f9596
Barun-Mishra-09 <144221171+Barun-Mishra-09@users.noreply.github.com>
2025-11-13 02:22:25 +0530

    Change single upload field name to 'profilePhoto'

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -2,7 +2,8 @@ import multer from "multer";
 
 const storage = multer.memoryStorage();
 
-export const singleUpload = multer({ storage }).single("file");
+// ✅ Change "file" → "profilePhoto"
+export const singleUpload = multer({ storage }).single("profilePhoto");
 
 export const multiUpload = multer({ storage }).fields([
   { name: "resume", maxCount: 1 },
